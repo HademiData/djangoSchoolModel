6c5bbb4d34e506b0da89e60dfeffd96266453714
Adewale Afolabi <120673579+AdewaleData@users.noreply.github.com>
2023-05-23 00:08:10 +0100

    Create SimpleSchDBmodel.py
    several models representing different entities within a school database.

---
+++ SimpleSchDBmodel.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    # Other fields related to the school
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    # Other fields related to the student
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    # Other fields related to the teacher
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    # Other fields related to the course
+
+    def __str__(self):
+        return self.name
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    # Other fields related to the enrollment
+
+    def __str__(self):
+        return f"{self.student} - {self.course}"
